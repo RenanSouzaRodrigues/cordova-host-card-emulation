c49c525466642b62fbc5c37795713ec35edfd104
Renan Souza <renan.souza@powerpush.com.br>
2023-11-02 00:41:43 -0300

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,23 @@
-declare module "cordova-host-card-emulation";
+declare module "cordova-host-card-emulation" {
+    /**
+     * Function to start listening for reader device
+     * @param successCallback Success callback
+     * @param errorCallback  Error callback
+     */
+    export function listenReaderDevice(successCallback: (data: any) => void, errorCallback: (err: any) => void): void;
+    
+    /**
+     * Function to stop listening for reader device
+     * @param successCallback Success callback
+     * @param errorCallback Error callback
+     */
+    export function stopListeningReaderDevice(successCallback: (data: any) => void, errorCallback: (err: any) => void): void;
+    
+    /**
+     * Function to send the response to the reader device. The response must be inside the listener event callback.
+     * @param response The response to send to the reader device in Uint8Array format
+     * @param successCallback 
+     * @param errorCallback 
+     */
+    export function sendResponseToReaderDevice(response: Uint8Array, successCallback: (data: any) => void, errorCallback: (err: any) => void): void;
+};
--- www/CordovaHostCardEmulation.js
+++ www/CordovaHostCardEmulation.js
@@ -2,14 +2,16 @@
 
 var cordova = require('cordova');
 
-exports.listenReaderDevice = function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "CordovaHostCardEmulation", "listenReaderDevice", []);
-},
+module.exports = {
+    listenReaderDevice: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "CordovaHostCardEmulation", "listenReaderDevice", []);
+    },
 
-exports.sendResponseToReaderDevice = function (response, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "CordovaHostCardEmulation", "sendResponseToReaderDevice", [response]);
-},
+    sendResponseToReaderDevice: function (response, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "CordovaHostCardEmulation", "sendResponseToReaderDevice", [response]);
+    },
 
-exports.stopListeningReaderDevice = function (successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "CordovaHostCardEmulation", "stopListeningReaderDevice", []);
+    stopListeningReaderDevice: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "CordovaHostCardEmulation", "stopListeningReaderDevice", []);
+    }
 }
