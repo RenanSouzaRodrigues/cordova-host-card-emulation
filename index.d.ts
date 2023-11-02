099ac8bca0c814bc6b21c5c3e81b7dcb9939e117
Renan Souza <renan.souza@powerpush.com.br>
2023-11-01 23:18:26 -0300

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,1 @@
-declare module "cordova-host-card-emulation" {
-    export function listenReaderDevice(successCallback: function, errorCallback: function): void;
-    export function sendResponse(response: string, successCallback: function, errorCallback: function): void;
-    export function stopListeningReaderDevice(successCallback: function, errorCallback: function): void;
-}
+declare module "cordova-host-card-emulation";
